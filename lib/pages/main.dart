ddcce97cd3d226882b25178be33a1ffdbdef70a2
Rotem Zwi <rotemzwi@gmail.com>
2021-02-15 17:51:25 +0200

    add components and pages folders

---
+++ lib/pages/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+
+  String titleText = "before";
+  void onPress() {
+    setState(() {
+      titleText = "clicked";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Almiya"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(titleText),
+              new RaisedButton(onPressed: onPress, child: new Text('click me'),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
