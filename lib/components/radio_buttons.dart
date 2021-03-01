4213266f25fab284fd3d526477db55ae73d0b847
barakgro <barakgrossman@gmail.com>
2021-03-01 19:15:22 +0200

    refactored radio buttons component

--- lib/components/radio_buttons.dart
+++ lib/components/radio_buttons.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-enum Option {yes, think_so, not_exactly}
-
-
 class RadioButtons extends StatefulWidget {
   RadioButtons({Key key}) : super(key: key);
 
@@ -12,72 +9,43 @@ class RadioButtons extends StatefulWidget {
 
 class RadioButtonsState extends State<RadioButtons> {
 
-  Option option = Option.think_so;
+  var textArray = ["כן", "נראה לי, אבל אני לא יודעת בדיוק לנסח אותה", "לא בדיוק"];
+  var valuesArray = ["yes", "think_so", "not_exactly"];
+  String selectedValue = "think_so";
+  void setSelectedValue(String value) => setState(() => selectedValue = value);
 
-  Widget build(BuildContext context) {
-
-    const TextStyle textStyle = TextStyle(
+  TextStyle textStyle = TextStyle(
       color: Color(0xff542d53),
       fontWeight: FontWeight.bold,
       fontSize: 18
-    );
+  );
 
-    return Column(
-      children: <Widget>[
-        RadioListTile(
-          title: const Text(
-            "כן",
-            textDirection: TextDirection.rtl,
-            style: textStyle,
-          ),
-          value: Option.yes,
-          groupValue: option,
+  Widget makeRadios() {
+    List<Widget> radioList = new List();
+    for(int i = 0; i < valuesArray.length; i++) {
+      radioList.add(
+        new RadioListTile(
+          value: valuesArray[i],
+          groupValue: selectedValue,
+          onChanged: setSelectedValue,
           controlAffinity: ListTileControlAffinity.trailing,
-          onChanged: (Option value) {
-            setState(() {
-              option = value;
-            });
-          },
-        ),
+          title: new Text(
+            textArray[i],
+            textDirection: TextDirection.rtl,
+            style: textStyle),
+        )
+      );
+      radioList.add(
         Divider(
           color: Color(0xff542d53),
           thickness: 2
-        ),
-        RadioListTile(
-          title: const Text(
-            "נראה לי, אבל אני לא יודעת בדיוק לנסח אותה",
-            textDirection: TextDirection.rtl,
-            style: textStyle,
-          ),
-          value: Option.think_so,
-          groupValue: option,
-          controlAffinity: ListTileControlAffinity.trailing,
-          onChanged: (Option value) {
-            setState(() {
-              option = value;
-            });
-          },
-        ),
-        Divider(
-            color: Color(0xff542d53),
-            thickness: 2
-        ),
-        RadioListTile(
-          title: const Text(
-            "לא בדיוק",
-            textDirection: TextDirection.rtl,
-            style: textStyle,
-          ),
-          value: Option.not_exactly,
-          groupValue: option,
-          controlAffinity: ListTileControlAffinity.trailing,
-          onChanged: (Option value) {
-            setState(() {
-              option = value;
-            });
-          },
-        ),
-      ],
-    );
+      ));
+    }
+    Column col = new Column(children: radioList);
+    return col;
+  }
+
+  Widget build(BuildContext context) {
+    return makeRadios();
   }
 }
